b0535d0c263f26a6adb1d643c8d1232f3f3d5c16
Matondo Antonio <matondo.antonio@ideiasdinamicas.com>
2022-07-20 16:59:33 +0100

    improved solution

--- Leetcode Learning Phase 1/binarySearch/744.FindSmallestLetterGreaterThanTarget/744.FindSmallestLetterGreaterThanTarget.js
+++ Leetcode Learning Phase 1/binarySearch/744.FindSmallestLetterGreaterThanTarget/744.FindSmallestLetterGreaterThanTarget.js
@@ -1,7 +1,6 @@
 
 function nextGreatestLetter(letters, target) {
    let left = 0, right = letters.length - 1
-   let word = ''
    while(left < right) {
        let middle = Math.floor((right + left) / 2)
        let targetCode = target.charCodeAt(0)
@@ -17,5 +16,23 @@ function nextGreatestLetter(letters, target) {
    let condition = letters[left].charCodeAt(0) <= target.charCodeAt(0)
    if(left >= right && condition) return letters[0]
    return letters[left]
+};
+
+
+//improved and most fast
+
+function nextGreatestLetter(letters, target) {
+   let left = 0, right = letters.length
+   while(left < right) {
+       let middle = Math.floor((right + left) / 2)
+       if(letters[middle] > target) {
+          right = middle 
+        }else {
+          left = middle + 1
+        }
+
+   }
+    
+   return letters[left % letters.length]
 };
 console.log(nextGreatestLetter(["c","f","j"], "d"))
